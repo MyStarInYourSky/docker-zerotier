2d68faf9db070ccc35070cfdb32b1b24db3b9480
starlightcrafted <endpoint_data250@starlightcrafted.com>
2024-11-10 18:21:12 +0000

    move away from earthly trial

--- buildscripts/merge.sh
+++ buildscripts/merge.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install Dependencies
-apt update && apt -y install jq curl
-pip3 install semver
+sudo apt update && apt -y install jq curl
+sudo pip3 install semver
 
 export LATEST_VERSION=$(curl -s -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/zerotier/ZeroTierOne/releases | jq -r '.[] | select(.prerelease == false)| select(.draft == false) | .tag_name' | sort -V | tail -1)
 export REGISTRY_VERSION=$(curl -s -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" -H "Authorization: Bearer ${gh_token}" https://api.github.com/orgs/MyStarInYourSky/packages/container/zerotier/versions | jq -r '.[].metadata.container.tags.[]| select(. != "latest")' | sort -V  | tail -1)
